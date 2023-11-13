c4c23a7601a016843a7adf3c31ce548012fe8834
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-13 20:37:43 +1100

    sudo

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,7 +13,7 @@ set -e
   #Create source distribution and a compressed archive of the project's source code.
   #Create a binary distribution of the project that can be installed on different platforms. 
 
-sh 'sudo pip install setuptools'
+sudo pip install setuptools
 sh 'python setup.py sdist bdist_wheel'
 
 # Move the artifacts to a designated directory
