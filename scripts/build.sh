8b9771f63863731c0a26691e064e5010d448c242
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-13 12:36:43 +1100

    build changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Create a virtual environment
-//sh 'python -m venv venv'
-//sh 'source venv/bin/activate'
+  #sh 'python -m venv venv'
+  #sh 'source venv/bin/activate'
+  #sh 'pip install Flask'
 
-//sh 'pip install Flask'
+# Install dependencies 
+  #sh 'pip install -r requirements.txt'
 
-// Install dependencies 
-//sh 'pip install -r requirements.txt'
-
-// Build a distribution package for a Python project that can be easily shared and installed by others.
-    // Create source distribution and a compressed archive of the project's source code.
-    // Create a binary distribution of the project that can be installed on different platforms. 
+# Build a distribution package for a Python project that can be easily shared and installed by others.
+  #Create source distribution and a compressed archive of the project's source code.
+  #Create a binary distribution of the project that can be installed on different platforms. 
+ 
 sh 'python setup.py sdist bdist_wheel'
 
-// Move the artifacts to a designated directory
+# Move the artifacts to a designated directory
 sh 'mkdir -p artifacts'
 sh 'mv dist/* artifacts/
                     
-// Archive the generated artifacts
+#Archive the generated artifacts
 archiveArtifacts artifacts: 'dist/*', fingerprint: true
