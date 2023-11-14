fe35f0f8c9bf23df60acee0bb63eebdd33258a93
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-14 21:19:45 +1100

    setuptools

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,12 +15,12 @@ sudo pip install -r requirements.txt
   #Create source distribution and a compressed archive of the project's source code.
   #Create a binary distribution of the project that can be installed on different platforms.
 
-#sudo python -m pip install setuptools
-#sudo python -m pip setup.py sdist bdist_wheel
+sudo python3 -s pip install setuptools
+sudo python3 -s pip setup.py sdist bdist_wheel
 
 # Move the artifacts to a designated directory
-#sudo -s mkdir -p artifacts
-#sudo -s mv dist/* artifacts/
+sudo -s mkdir -p artifacts
+sudo -s mv dist/* artifacts/
                     
 #Archive the generated artifacts
-#sudo -s archiveArtifacts artifacts: 'dist/*', fingerprint: true
+sudo -s archiveArtifacts artifacts: 'dist/*', fingerprint: true
