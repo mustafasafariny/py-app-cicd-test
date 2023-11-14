440e60615a1ae8b98674244dfca3cdcbcf9e4ee1
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-14 11:50:11 +1100

    sudo

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,23 +2,23 @@
 set -e
 
 # Create a virtual environment
-python3 -m venv venv
-source venv/bin/activate
-pip install Flask
+sudo python3 -m venv venv
+sudo source venv/bin/activate
+sudo pip install Flask
 
 # Install python pacakges and dependencies 
-pip install -r requirements.txt
+sudo pip install -r requirements.txt
 
 # Build a distribution package for a Python project that can be easily shared and installed by others.
   #Create source distribution and a compressed archive of the project's source code.
   #Create a binary distribution of the project that can be installed on different platforms.
 
-pip install setuptools
-python setup.py sdist bdist_wheel
+sudo pip install setuptools
+sudo python setup.py sdist bdist_wheel
 
 # Move the artifacts to a designated directory
-mkdir -p artifacts
-mv dist/* artifacts/
+sudo mkdir -p artifacts
+sudo mv dist/* artifacts/
                     
 #Archive the generated artifacts
-archiveArtifacts artifacts: 'dist/*', fingerprint: true
+sudo archiveArtifacts artifacts: 'dist/*', fingerprint: true
