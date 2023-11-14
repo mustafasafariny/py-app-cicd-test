b43549f01e422d73590efef43083cb53aa92c16d
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-14 15:39:08 +1100

    xxx

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ sudo -s source venv/bin/activate
 sudo -s pip3 install Flask
 
 # Install python pacakges and dependencies 
-sudo python install -r requirements.txt
+#sudo python install -r requirements.txt
 
 # Build a distribution package for a Python project that can be easily shared and installed by others.
   #Create source distribution and a compressed archive of the project's source code.
