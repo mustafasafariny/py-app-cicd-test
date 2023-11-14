300586bffe899ac57336fc3110a8c0387182df61
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-14 15:30:47 +1100

    pip

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,12 @@
 set -e
 
 # Create a virtual environment
-sudo -s python -m venv venv
+sudo -s python3 -m venv venv
 sudo -s source venv/bin/activate
-sudo -s python install Flask
+sudo -s pip3 install Flask
 
 # Install python pacakges and dependencies 
-sudo -s python install -r requirements.txt
+sudo python install -r requirements.txt
 
 # Build a distribution package for a Python project that can be easily shared and installed by others.
   #Create source distribution and a compressed archive of the project's source code.
