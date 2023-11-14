1d6242e214f7c6dc1f5a64a00d96e775a6cc3bdb
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-15 10:06:26 +1100

    SETUP

--- setup.py
+++ setup.py
@@ -7,9 +7,14 @@ setup(
     author="Mustafa",
     author_email="mustafa.mustafa@domain.com.au",
     description="package for python application",
+#    url="https://pypi.example.org/pypi/somedep/",
     install_requires=[
         # List your project dependencies here
     ],
+#    data_files=[
+#    ('.../MyProgram/img/logo.ico'),
+#    ('.../MyProgram/media/head.txt'),
+#    ],
 #    entry_points={
 #        'console_scripts': [
 #            'your_script = your_module.your_script:main',
