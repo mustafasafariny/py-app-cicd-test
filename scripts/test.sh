a125d346a81740a23962fb716f8df29493b98854
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-16 22:28:25 +1100

    test flask server

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,5 @@ set -e
 #discover and run all unit tests in the current directory and its subdirectories. 
 sudo -s python3 -m unittest discover
 
+#Rnning app on the web flask server
 sudo -s python3 src/demo-py-app/main.py
