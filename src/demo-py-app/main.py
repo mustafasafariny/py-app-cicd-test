6c6338def009e4b70ccf164a1f4963024eed70ea
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-16 11:12:32 +1100

    testing py app

---
+++ src/demo-py-app/main.py
@@ -0,0 +1,22 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+#Flask uses @app.route to connect URL endpoints with code contained in functions.
+#The argument to @app.route defines the URL’s path component, which is the root path ("/") in this case.
+#function def index() defines what should be executed if the defined URL endpoint is requested by a user and when the  page is loaded
+
+#In other words, if a user types the base URL of your web app into their browser, then Flask runs index() 
+# and the user sees the returned text.
+@app.route("/")
+def index():
+    return "Congratulations, it's a Python web app!!"
+
+#Tell Python to start Flask’s development server when the script is executed from the command line. 
+#It’ll be used only when you run the script locally
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
+
+# To test on Flask server (using PowerSell Terminal)
+# Within the created virtual env (.venv) Navigate to src lib aapplication folder and $ flask --app main.py run
