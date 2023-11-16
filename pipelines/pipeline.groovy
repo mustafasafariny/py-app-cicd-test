33fb4d1fcb1504919216b92e589d89ac5c6d8789
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-16 13:30:31 +1100

    aaa

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -6,9 +6,11 @@ pipeline {
         choice(name: 'VERSION', choices: ['1.1.0', '1.2.0', '1.3.0'], description: '')
         booleanParam(name: 'executeTests', defaultValue: true, description: '')
     }
+    
     //tools{
     //    maven 'Maven'
     //}
+
     environment {
         DISABLE_AUTH = 'true'
         NEW_VERSION = '1.0.0'
