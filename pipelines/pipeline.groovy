ef1af92d5a8cd0e3f1683588f84529340ab92be1
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-16 13:18:47 +1100

    param

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -4,7 +4,7 @@ pipeline {
     parameters{
         string(name: 'VERSION', defaultValue: '', description: 'version to deploy')
         choice(name: 'VERSION', choices: ['1.1.0', '1.2.0', '1.3.0'], description: '')
-        pooleanParm: 'executeTests', defaultValue: true, description: '')
+        pooleanParm(name: 'executeTests', defaultValue: true, description: '')
     }
     //tools{
     //    maven 'Maven'
