4b8bdc5fae70bd8b8ff583116c3a9f7939c17197
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-19 18:06:51 +1100

    zzzz

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -56,9 +56,9 @@ pipeline {
 
                             {   dir('./s3-cdk') {
                                     echo 'changed dir'
+                                    sh 'sudo npm install'
                             }                                
-                                sh 'chmod +x ./s3-cdk/cdk-scripts/cdkappbuild.sh'
-                                sh 'sudo npm install'
+                                sh 'chmod +x ./s3-cdk/cdk-scripts/cdkappbuild.sh'                              
                                 sh './s3-cdk/cdk-scripts/cdkappbuild.sh'
 
                                 echo "before s3 upload...!"
