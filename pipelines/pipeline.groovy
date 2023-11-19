02a10175b723ed693df9a98a1fe849d58f1e05d7
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-19 18:32:35 +1100

    aa

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -56,7 +56,7 @@ pipeline {
 
                             {   dir('./s3-cdk') {
                                     echo 'changed dir'
-                                    sh 'sudo -s /root/.nvm/versions/node/v6.10.0/bin/npm install'
+                                    sh 'sudo -s npm install'
                             }                                
                                 sh 'chmod +x ./s3-cdk/cdk-scripts/cdkappbuild.sh'                              
                                 sh './s3-cdk/cdk-scripts/cdkappbuild.sh'
