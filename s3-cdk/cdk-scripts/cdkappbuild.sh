613c5a72e388d9b2d05ae618ca8e704c0822e6fd
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-19 16:23:08 +1100

    sudo

--- s3-cdk/cdk-scripts/cdkappbuild.sh
+++ s3-cdk/cdk-scripts/cdkappbuild.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # Typescript dependencies installation (packages and lib/modules)
-sh 'echo cdk infra build script..'
-sh 'npm install -g typescript'
-sh 'npm install'
-sh 'npm install @aws-cdk/core @aws-cdk/aws-s3'
-sh 'npm install aws-cdk-lib/aws-iam'
-sh 'npm install aws-cdk-lib/aws-iam'
+sh 'sudo echo cdk infra build script..'
+sh 'sudo npm install -g typescript'
+sh 'sudo npm install'
+sh 'sudo npm install @aws-cdk/core @aws-cdk/aws-s3'
+sh 'sudo npm install aws-cdk-lib/aws-iam'
+sh 'sudo npm install aws-cdk-lib/aws-iam'
 #sh 'tsc'
 #cdk synth
-sh 'npm run build'
-sh 'npm run cdk synth' 
+sh 'sudo npm run build'
+sh 'sudo npm run cdk synth' 
