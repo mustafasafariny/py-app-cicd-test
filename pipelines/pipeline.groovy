f06852a24a29e775f138bb02e647b19284edf882
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-20 14:51:20 +1100

    ccc

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -68,9 +68,6 @@ pipeline {
                                 sh 'sudo apt remove nodejs-doc -y'
                                 sh 'sudo dpkg --remove --force-remove-reinstreq libnode-dev'
                                 sh 'sudo dpkg --remove --force-remove-reinstreq libnode72:amd64'
-                                //sh 'sudo npm ls -g --depth=0 | awk -F/ '/node_modules/ && !/\/npm$/ {print $NF}' | xargs npm -g rm'
-                                //sh 'sudo npm ls --depth=0 | awk -F/ '/node_modules/ && !/\/npm$/ {print $NF}' | xargs npm rm'
-                                //sh 'sudo npm uninstall `ls -1 node_modules | tr '/\n' ' '`'
 
                                 sh 'sudo apt install -y curl software-properties-common'
                                 sh 'curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -'
@@ -80,9 +77,9 @@ pipeline {
                                 echo 'Node.js -v && npm --version'
                                                                 
                                 sh 'sudo npm install -g typescript'
-                                                              
-                                sh 'chmod +x ./s3-cdk/cdk-scripts/cdkappbuild.sh'                              
-                                sh './s3-cdk/cdk-scripts/cdkappbuild.sh'
+
+                                sh 'chmod +x ./cdkappbuild.sh'                              
+                                sh './cdkappbuild.sh'
 
                                 echo "before s3 upload...!"
                                 s3Upload(file: "${ARTIFACTS_FILE}",
