713b66591d594fc498415a7f6972db3905f7484a
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-21 16:37:28 +1100

    bootstrap

--- s3-cdk/cdk-scripts/cdkappbuild.sh
+++ s3-cdk/cdk-scripts/cdkappbuild.sh
@@ -5,8 +5,18 @@ set -e
 echo 'cdk build start...'
 npm run build
 
+echo 'cdk bootstrap start...'
+cdk bootstrap
+
 echo 'cdk synth start...'
 npm run cdk synth
 
 #sudo -s 'npm run build && cdk synth --all'
 #sudo -s 'cdk destroy'
+
+#Bootstrap 
+#Bootstrapping is the process of provisioning resources for the AWS CDK before you can deploy AWS CDK apps into an AWS environment.
+#(An AWS environment is a combination of an AWS account and Region).
+#These resources include an Amazon S3 bucket for storing files and IAM roles that grant permissions needed to perform deployments.
+#The required resources are defined in an AWS CloudFormation stack, called the bootstrap stack, which is usually named CDKToolkit.
+#Like any AWS CloudFormation stack, it appears in the AWS CloudFormation console once it has been deployed.
