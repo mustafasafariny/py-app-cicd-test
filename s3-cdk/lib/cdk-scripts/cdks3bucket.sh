e3d2bce3d406cda69a495aabba7106510dae6c26
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-22 14:39:20 +1100

    xxx

--- s3-cdk/lib/cdk-scripts/cdks3bucket.sh
+++ s3-cdk/lib/cdk-scripts/cdks3bucket.sh
@@ -3,13 +3,10 @@ set -e
 # Typescript dependencies installation (packages and lib/modules)
 
 echo 'cdk build start...'
-sh 'sudo tsc'
-#npm run build
+npm run build
 
 echo 'cdk synth start...'
-sh 'sudo cdk synth'
-#npm run cdk synth
+npm run cdk synth
 
 echo 'cdk deploy start...'
-sh 'sudo cdk deploy'
-#npm run cdk deploy
+npm run cdk deploy
