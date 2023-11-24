767721a0910a05a8ca80e45f2067c2404d384ec6
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-24 12:29:35 +1100

    tsc

--- cdk-infra/lib/cdk-scripts/cdks3bucket.sh
+++ cdk-infra/lib/cdk-scripts/cdks3bucket.sh
@@ -2,8 +2,8 @@
 set -e
 # Typescript dependencies installation (packages and lib/modules)
 
-#echo 'cdk build start...'
-#npm run build
+echo 'cdk build start...'
+npm run build
 
 echo 'cdk synth start...'
 npm run cdk synth
