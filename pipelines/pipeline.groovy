df34c408e41bbcd3c8851ae250341273af9d5b4d
Mustafa <mustafa.mustafa@domain.com.aum>
2023-11-27 11:20:30 +1100

    xxx

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -75,7 +75,7 @@ pipeline {
                                 sh 'sudo npm install -g typescript aws-cdk'
 
                                 echo 'build & deploy stack'
-                                sh 'tsc @args --sourceRoot ./lib/src'
+                                sh 'tsc  --verbose --diagnostics'
                                 //sh 'tsc'
                                 sh 'cdk synth'
                                 //sh 'cdk bootstrap aws://144358027444/'ap-sountheast-2'
