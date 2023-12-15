1fb24c31744ebbe87f1e91709664dc88a7d694f3
Mustafa <mustafa.mustafa@domain.com.aum>
2023-12-15 13:00:16 +1100

    zzz

--- deployment/lib/cdk-scripts/cdks3bucket.sh
+++ deployment/lib/cdk-scripts/cdks3bucket.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-sudo apt update -y
-sudo apt install -y curl software-properties-common
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get update
-sudo apt-get install -y nodejs
+# sudo apt update -y
+# sudo apt install -y curl software-properties-common
+# curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+# sudo apt-get update
+# sudo apt-get install -y nodejs
 npm install
-npm run build
-
+#npm run build
+tsc
 pushd  ./deployment/bin                                 
     cdk synth --app "npx ts-node deployment.js" CdkInfraAppCodeStack
     cdk deploy --app "npx ts-node deployment.js" CdkInfraAppCodeStack
