e9fcdbdeff47e7aab9fd0e7f56f373d80640b460
Mustafa <mustafa.mustafa@domain.com.aum>
2023-12-18 14:46:55 +1100

    aaa

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -80,20 +80,11 @@ pipeline {
             steps {
                 echo 'Deploying...'
                 echo " Deployment environment is ${params.Env}"
-                sh 'sudo cdk deploy /var/lib/jenkins/workspace/pyapp-test-pipeline/deployment/bin/cdk-infra-app-code.js'
-            /*
-                sh '''
-                    pwd
-                    cd deployment/bin
-                    pwd
-                    sudo cdk deploy --app "npx ts-node cdk-infra-app-code.js" CdkInfraAppCodeStack
-                '''
-            */
 
-            /*    script {
+                script {
                     sh './deployment/lib/cdk-scripts/deploys3stack.sh'                     
                     }
-            */       
+                  
                 }  
             }
 
