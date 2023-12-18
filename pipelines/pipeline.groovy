2d9f423a0ebb30e6635288349cda3b8e9a2d6d81
Mustafa <mustafa.mustafa@domain.com.aum>
2023-12-19 09:55:03 +1100

    aaa

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -84,16 +84,15 @@ pipeline {
                 script {
                     // withCredentials([sshUserPrivateKey(credentialsId: 'awssshcredentials', keyFileVariable: 'SSH_KEY')])
                     // withAWS(credentials:'awscrd', , region: 'ap-sountheast-2')
-                    
+
                     withAWS(role:'AWS-DevOps-Identity', roleAccount:'144358027444')
                        {
-                        sh'''
-                        cd
-                        cd /var/lib/jenkins/workspace/pyapp-test-pipeline/deployment/bin
-                        sudo cdk deploy --app "npx ts-node cdk-infra-app-code.js" CdkInfraAppCodeStack --profile cdk-sandpit -v
-
-                        '''
-                        //sh './deployment/lib/cdk-scripts/deploys3stack.sh'                     
+                    //    sh'''
+                    //    cd
+                    //    cd /var/lib/jenkins/workspace/pyapp-test-pipeline/deployment/bin
+                    //    sudo cdk deploy --app "npx ts-node cdk-infra-app-code.js" CdkInfraAppCodeStack --profile cdk-sandpit -v
+                    //    '''
+                        sh './deployment/lib/cdk-scripts/deploys3stack.sh'                     
                         }
                     } 
                 }  
