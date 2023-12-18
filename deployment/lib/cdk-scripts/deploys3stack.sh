fe5ec6230e4faf789a7cdeb008569e1c257e9db0
Mustafa <mustafa.mustafa@domain.com.aum>
2023-12-18 14:49:56 +1100

    aaaa

--- deployment/lib/cdk-scripts/deploys3stack.sh
+++ deployment/lib/cdk-scripts/deploys3stack.sh
@@ -4,6 +4,6 @@ set -e
 echo 'deploy.sh start'
 pushd  ./deployment/bin
     pwd 
-    cdk deploy --app "npx ts-node cdk-infra-app-code.js" CdkInfraAppCodeStack
+    cdk deploy --app -v "npx ts-node cdk-infra-app-code.js" CdkInfraAppCodeStack
 popd
 echo 'deploy.sh end'    
