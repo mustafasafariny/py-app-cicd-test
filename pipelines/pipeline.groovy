4d1a0f5f633baa1d38805c4a831132982800ae6b
Mustafa <mustafa.mustafa@domain.com.aum>
2023-12-18 17:06:41 +1100

    aaa

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -92,7 +92,7 @@ pipeline {
                         sh 'pwd'
                         cd /var/lib/jenkins/workspace/pyapp-test-pipeline/deployment/bin
                         sh 'pwd'
-                        sh' sudo cdk deploy --app "npx ts-node cdk-infra-app-code.js" CdkInfraAppCodeStack -v'
+                        sh 'sudo cdk deploy --app "npx ts-node cdk-infra-app-code.js" CdkInfraAppCodeStack -v'
                         //sh './deployment/lib/cdk-scripts/deploys3stack.sh'                     
                         }
                     } 
