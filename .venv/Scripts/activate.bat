04248856518118b01d9cf6292108a6e2702e8dd7
Mustafa <mustafa.mustafa@domain.com.aum>
2023-12-23 17:55:56 +1100

    all

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\MustafaMustafa\py_cicd_demo\py-app-cicd-test\.venv
+set VIRTUAL_ENV=c:\Users\MustafaMustafa\py_cicd_demo\py-app-cicd-test\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
