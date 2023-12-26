1757926810d49ec012f3351a4164f638e2a835fe
Mustafa <mustafa.mustafa@domain.com.aum>
2023-12-26 22:50:06 +1100

    aaa

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -87,17 +87,17 @@ pipeline {
                 echo "Access Key ID: ${env.AWS_ACCESS_KEY_ID}"
                 echo "Access Key Secret: ${env.AWS_SECRET_ACCESS_KEY}"
 
-                //withCredentials([string(credentialsId: 'mustafa-aws-creds', variable: 'AWS_SESSION_TOKEN')])
-                withCredentials([aws(accessKeyVariable:'AWS_ACCESS_KEY_ID', credentialsid:'mustafa-aws-cli-creds', 
-                                    secretKeyVariable:'AWS_SECRET_ACCESS_KEY')])
-                    {
+                //withCredentials([string(credentialsId: 'mustafa-aws-cli-creds', variable: 'AWS_SESSION_TOKEN')])
+                //withCredentials([aws(accessKeyVariable:'AWS_ACCESS_KEY_ID', credentialsid:'mustafa-aws-cli-creds', 
+                //                    secretKeyVariable:'AWS_SECRET_ACCESS_KEY')])
+                //    {
                     sh '''
                         aws --version
                         aws sts get-caller-identity
                         aws ec2 describe-instances
                         #aws s3 cp --sse AES256 file.txt s3://mus.cicd.cdk.demo/
                     '''
-                    }
+                //    }
 
                 //withAWS(credentials: 'awscredentials', profile: 'cdk-sandpit', region: 'ap-southeast-2', role: 'AWS-DevOps-Identity', roleAccount: '144358027444') 
                 //    {
