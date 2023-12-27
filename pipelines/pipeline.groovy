3eab732c28e940278c0ce4880c7f57b5287455a4
Mustafa <mustafa.mustafa@domain.com.aum>
2023-12-27 23:38:09 +1100

    aaa

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -93,7 +93,7 @@ pipeline {
                         #aws ec2 describe-instances
                         #aws s3 ls
 
-                        cd artifacts
+                        #cd artifacts
                         #aws s3 cp *.whl s3://mus.cicd.cdk.demo/py-app-artifacts/
                         aws s3 cp mustest1.txt s3://mus.cicd.cdk.demo/py-app-artifacts/
 
