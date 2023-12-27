986ca9ff6887b0060566fffb2c8d446160a03cfa
Mustafa <mustafa.mustafa@domain.com.aum>
2023-12-27 18:35:44 +1100

    aaa

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -87,7 +87,7 @@ pipeline {
                         aws sts get-caller-identity
                         #aws ec2 describe-instances
                         pwd
-                        #aws s3 ls
+                        aws s3 ls
 
                         cd ./src/demo-py-app
                         aws s3 cp artifacts/*.* s3://mus.cicd.cdk.demo/
