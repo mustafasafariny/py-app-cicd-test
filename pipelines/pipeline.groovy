585a5a5aee75d94e202b99455180f977ad714569
Mustafa <mustafa.mustafa@domain.com.aum>
2023-12-28 21:02:50 +1100

    aaa

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -80,8 +80,8 @@ pipeline {
 
                 echo 'Uploading S3 Bucket...'
 
-                withCredentials([aws(credentialsId: 'mus-jenkins-cicd-demo', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')])
-                    {
+                //withCredentials([aws(credentialsId: 'mus-jenkins-cicd-demo', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')])
+                //    {
                     sh '''
                         cd ./src/demo-py-app
                         pwd
@@ -100,7 +100,7 @@ pipeline {
                         #aws s3 cp mustest1.txt s3://mus.cicd.cdk.demo/py-app-artifacts/
 
                     '''
-                    }
+                //    }
 
                 //withAWS(credentials: 'awscredentials', profile: 'cdk-sandpit', region: 'ap-southeast-2', role: 'AWS-DevOps-Identity', roleAccount: '144358027444') 
                 //    {
