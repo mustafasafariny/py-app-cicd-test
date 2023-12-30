a710e4f3d759f9d7c66563962010e7bb8a7ca361
Mustafa <mustafa.mustafa@domain.com.aum>
2023-12-30 21:03:42 +1100

    aaa

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -87,10 +87,10 @@ pipeline {
                         cd ./src/demo-py-app/artifacts
                         aws --version
                         aws sts get-caller-identity
-                        aws ec2 describe-instances --region=ap-southeast-2
-                        aws s3 ls
+                        #aws ec2 describe-instances --region=ap-southeast-2
+                        #aws s3 ls
 
-                        s3Upload(bucket:"mus.cicd.cdk.demo", workingDir:'artifacts', includePathPattern:'**/*');
+                        s3Upload(bucket:"mus.cicd.cdk.demo", workingDir:'artifacts', includePathPattern:'**/*')
                         #s3Upload(file: 'file.txt', bucket: 'my-bucket', tags: '[tag1:value1, tag2:value2]')
 
                         #def tags=[:]
