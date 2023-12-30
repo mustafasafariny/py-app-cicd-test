1944673a24479a46b0cd9b4a9df69d7615428337
Mustafa <mustafa.mustafa@domain.com.aum>
2023-12-30 12:08:51 +1100

    aaa

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -95,8 +95,9 @@ pipeline {
                         aws ec2 describe-instances --region=ap-southeast-2
                         aws s3 ls
 
-                        aws s3 cp artifacts/* s3://mus.cicd.cdk.demo/py-app-artifacts/
-                        #aws s3 cp *.whl s3://mus.cicd.cdk.demo/py-app-artifacts/
+                        #aws s3 cp artifacts/* s3://mus.cicd.cdk.demo/py-app-artifacts/
+                        aws s3 cp *.whl s3://mus.cicd.cdk.demo/py-app-artifacts/
+                        aws s3 cp *.gz s3://mus.cicd.cdk.demo/py-app-artifacts/
                         #aws s3 cp *.whl s3://mus.cicd.cdk.demo/py-app-artifacts/  --profile cdk-sandpit
                         #aws s3 cp mustest1.txt s3://mus.cicd.cdk.demo/py-app-artifacts/
 
