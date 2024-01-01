6a8525b7de6dc890fa6ff0cee8c58dcea25a101c
Mustafa <mustafa.mustafa@domain.com.aum>
2024-01-01 14:51:54 +1100

    aaa

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -19,7 +19,6 @@ pipeline {
         AWS_DEFAULT_ACCOUNT = '144358027444'
         AWS_ROLE = 'AWS-DevOps-Identity'
         AWS_S3_BUCKET = 'mus.cicd.cdk.demo'      
-        ARTIFACTS_DIR = 'artifacts'
 
     }
 
@@ -34,9 +33,10 @@ pipeline {
                     sh 'chmod +x ./src/scripts/build.sh'
                 }
                 
-                // Create Aritcats files in different formats
+                //Create Aritcats files in different formats
                 //while tar.gz is often used for distributing source code that needs to be built, 
                 //the wheel format is used for distributing pre-built binary packages that can be installed more quickly. 
+
                 archiveArtifacts artifacts: 'src/demo-py-app/artifacts/*.tar.gz, src/demo-py-app/artifacts/*.whl', fingerprint: true
             }
         }
@@ -70,41 +70,26 @@ pipeline {
                 echo " Deployment environment is ${params.Env}"
 
                 script {
-                        sh 'chmod +x ./src/scripts/deploy.sh'
-                        
-                        //def tags=[:]
-                        //    tags["tag1"]="${env.BUILD_TAG}"
-                        //    tags["tag2"]="mustafa"          
+                        sh 'chmod +x ./src/scripts/deploy.sh'      
                         }
 
                 echo 'Uploading S3 Bucket...'
 
-                //withAWS(roleAccount:'144358027444', role:'AWS-DevOps-Identity')
+                //Option 1 - using AWS CLI cp api to copy both artifacts to the S3 Bucket
+                //Option 2 - using Jenkins Pipeline: AWS Steps Plugin (preffered)
+
                 withAWS(roleAccount:"${AWS_DEFAULT_ACCOUNT}", role:"${AWS_ROLE}", region: "${AWS_DEFAULT_REGION}" )
                     {
-                    sh '''
-                        cd ./src/demo-py-app/artifacts
-                        aws --version
-                        #aws sts get-caller-identity
-                        #aws ec2 describe-instances --region=ap-southeast-2
-                        #aws s3 ls
-                        #Option 1 - using AWS CLI cp api to copy artifacts to 3 Bucket
-                        #aws s3 cp *.whl s3://mus.cicd.cdk.demo/py-app-artifacts/
-                        #aws s3 cp *.gz s3://mus.cicd.cdk.demo/py-app-artifacts/
-                        
-                    '''
-                    echo 'before upload...'
-
-                    // Option 2 - using Jenkins Pipeline: AWS Steps Plugin
-                    s3Upload(
-                        file: "./src/demo-py-app/artifacts/",
-                        bucket: 'mus.cicd.cdk.demo',
-                        path: 'py-app-artifacts/',
-                        tags: '[tag1:${env.BUILD_TAG}, tag2:${env.GIT_BRANCH}]',
-                        metadatas: ["repo:${env.JOB_NAME}", "branch:${env.BRANCH}", "commit:${env.GIT_COMMIT}"]
+                        //sh 'chmod +x ./src/scripts/deploy.sh'   // option 1                   
+                    
+                        s3Upload(
+                            file: "./src/demo-py-app/artifacts/",
+                            bucket: 'mus.cicd.cdk.demo',
+                            path: 'py-app-artifacts/',
+                            tags: '[tag1:${env.BUILD_TAG}, tag2:${env.GIT_BRANCH}]',
+                            metadatas: ["repo:${env.JOB_NAME}", "branch:${env.BRANCH}", "commit:${env.GIT_COMMIT}"]
                         )
 
-                    echo 'after upload...'
                     }                     
                 }
         }
--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -2,5 +2,9 @@
 set -e
 
 echo 'deploy.sh start'
+cd ./src/demo-py-app/artifacts
+
+aws s3 cp *.whl s3://mus.cicd.cdk.demo/py-app-artifacts/
+aws s3 cp *.gz s3://mus.cicd.cdk.demo/py-app-artifacts/
 
 echo 'deploy.sh end'    
