d0cab32731a2e54df5c7d4dfb5aae5e914179192
Mustafa <mustafa.mustafa@domain.com.aum>
2024-01-01 17:48:28 +1100

    aaa

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -53,8 +53,10 @@ pipeline {
                  
                 script {
                     pwd
+                    cd ./src/demo-py-app
+                    pwd
                     sh 'pytest flaskapp.py'
-                    sh 'chmod +x ./src/scripts/test.sh'
+                    #sh 'chmod +x ./src/scripts/test.sh'
                 }
 
             }
